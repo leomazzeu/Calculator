4a10e0749d2b8f0ca56c8d9d16f6f13f2d1c7b0c
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-19 11:24:10 -0300

    Função de Soma

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+
+            Console.ReadLine();
         }
     }
 }
