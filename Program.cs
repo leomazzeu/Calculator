79af59e57a235628c2749d9746b5b996649d3807
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-19 11:26:57 -0300

    Utilizando a função SOMA

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+        {
             Console.WriteLine("Primeiro valor:");
             float v1 = float.Parse(Console.ReadLine());
 
